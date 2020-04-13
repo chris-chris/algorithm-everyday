603ef00b07667635b23b155e0c420d4d2f07c203
Hoyean Song <sjhshy@gmail.com>
2020-04-14 00:03:51 +0900

    update 3

---
+++ shortest_path/dijikstra3.py
@@ -0,0 +1,55 @@
+import collections
+
+graph = collections.defaultdict(list)
+
+n, m = input().split()
+n, m = int(n), int(m)
+
+table = [999999999 for _ in range(n)]
+check = [False for _ in range(n)]
+
+for _ in range(m):
+  s, e = input().split()
+  s, e = int(s), int(e)
+  graph[s].append(e)
+  graph[e].append(s)
+
+start, to = input().split()
+start, to = int(start), int(to)
+
+table[start] = 0
+
+for i in range(n):
+  min_index = -1
+  min_value = 999999999
+  if min_value >= table[i] and not check[i]:
+    min_value = table[i]
+    min_index = i
+
+  check[min_index] = True
+
+  for target in graph[min_index]:
+    if table[target] >= table[min_index] + 1:
+      table[target] = table[min_index] + 1
+
+print(f"{table[to]}")
+
+
+"""
+11 14
+0 1
+0 2
+1 2
+1 4
+1 5
+2 3
+3 7
+4 7
+4 9
+4 10
+5 6
+6 8
+6 10
+7 8
+0 10
+"""
