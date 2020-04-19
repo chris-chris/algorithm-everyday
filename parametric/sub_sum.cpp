b7d9bac8c7697d8ddef53d373cb2e5f665df3bb1
Hoyean Song <sjhshy@gmail.com>
2020-04-20 05:59:35 +0900

    union sum

--- parametric/sub_sum.cpp
+++ parametric/sub_sum.cpp
@@ -5,44 +5,82 @@ const int MAX = 10100;
 long long int n, i, maxn=0;
 long long int s[MAX], e[MAX];
 
-long long int count(long long int idx){
-
+long long int count(long long int number){
+  // input number:
+  // 1 4
+  // 3 5
+  // number: 4
+  // return: 1~4 => 4
+  // 3~4 => 2
+  // 6 numbers smaller than or equal to 4
   long long int cnt = 0;
-  for(long long int x=0;x<n;x++){
-    long long int end = idx;
-    if(e[x] < idx)
-      end = e[x];
-    if(idx - s[x] > 0)
-      cnt += end - s[x];
+  for(int i=0;i<n;i++){
+
+    long long int number_or_end = number;
+    if(number >= e[i]) number_or_end = e[i];
+
+    long long int diff = number_or_end - s[i] + 1;
+    if(diff > 0)
+      cnt += diff;
+
   }
-  cout << idx << " " << cnt << endl;
   return cnt;
 }
 
 int main() {
+  long long int maxe=0;
   cin >> n;
-  for(int x=0;x<n;x++){
-    cin >> s[x] >> e[x];
-    if(maxn < e[x]){
-      maxn = e[x];
-    }
+  long long int target;
+  for(int i=0;i<n;i++){
+    cin >> s[i] >> e[i];
+    if(e[i]>=maxe)
+      maxe = e[i];
   }
-  cin >> i;
-  long long int p=1, q=maxn;
+  cin >> target;
+  // 1 4
+  // 3 5
+  // 3
+
+  // 1 2 3 4
+  // 3 4 5
+  // 1 2 3 3 4 4 5
+  // 0 1 2 3
+  // => 3
+  long long int p, q, cnt, result;
+  p = 1;
+  q = maxe;
+  cnt = 0;
+  result = 0;
+  cnt = count(p);
+  // p=1 q=5
+  //
   while(p+1<q){
-    long long int mid = (p+q)/2;
-    long long int cnt = count(mid);
-    if(cnt < i)
-      p = mid;
-    else if(cnt > i)
-      q = mid;
-    else{
-      p = mid;
-      break;
-    }
+     long long int mid = (p+q)/2;
+     // p=1 q=5 mid=3
+     cnt = count(mid); // count(3) = 4
+     if(cnt == target+1) { // exact
+       // cout << "exact" << mid << " " << cnt << " " << target+1 << endl;
+       p = mid;
+       break;
+     }else if(cnt < target+1){
+       // cout << "cnt < target " << p << " " << q << " " << mid << " " << cnt << " " << target+1 << endl;
+       p = mid;
+     }else{
+       // cout << "cnt > target " << p << " " << q << " " << mid << " " << cnt << " " << target+1 << endl;
+       q = mid;
+     }
+     // 1 2 3
+     // 2 3 4 5 6 7 8 9 10
+     // 1 2 3 4 5 6 7 8
+     // 1 1 2 2 2 3 3 3 4 4 5 5 ...
+     // 0 1 2 3 4 5 6 7 8 9 10 11
+
 
   }
-  cout << p << endl;
+  if(p+1 == q)
+    cout << p+1 << endl;
+  else
+    cout << p << endl;
 
   return 0;
 }
